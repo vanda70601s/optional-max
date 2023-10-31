64ff44bf6f8f89b024b5d8735115235b2bce9183
Work <vanda70601s@mail.ru>
2023-10-31 23:14:37 +0300

    Commit

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -1,9 +1,17 @@
 package com.epam.rd.autotasks.max;
 
+import java.util.Arrays;
 import java.util.OptionalInt;
 
 public class MaxMethod {
     public static OptionalInt max(int[] values) {
-        throw new UnsupportedOperationException();
-    }
+        OptionalInt result;
+        if (values == null || values.length == 0) {
+            result = OptionalInt.empty();
+        } else {
+            int max = Arrays.stream(values).max().getAsInt();
+            result = OptionalInt.of(max);
+        }
+        return result;
+        }
 }
